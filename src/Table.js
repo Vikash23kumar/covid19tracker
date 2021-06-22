e0d4f166bcff99568e7462bb1f67c33570e293ea
Vikash23kumar <54552695+Vikash23kumar@users.noreply.github.com>
2021-06-22 21:06:11 +0530

    first commit

---
+++ src/Table.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Table.css";
+import numeral from "numeral";
+
+const Table = (props) => {
+  const { countries } = props;
+  return (
+    <div className="table">
+      {countries.map((country) => {
+        return (
+          <tr>
+            <td>{country.country}</td>
+            <td>
+              <strong>{numeral(country.cases).format("0,0")}</strong>
+            </td>
+          </tr>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Table;
